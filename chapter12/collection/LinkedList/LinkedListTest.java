18261f6f7d26f6576386a4a30d37c3c743980062
Gunter <82664008+ramses203@users.noreply.github.com>
2021-07-19 10:44:26 +0900

    Add files via upload

---
+++ chapter12/collection/LinkedList/LinkedListTest.java
@@ -0,0 +1,26 @@
+package collection.LinkedList;
+
+import java.util.LinkedList;
+
+public class LinkedListTest {
+
+	public static void main(String[] args) {
+		
+		LinkedList<String> myList = new LinkedList<>();
+		
+		myList.add("A");
+		myList.add("B");
+		myList.add("C");
+		
+		System.out.println(myList);
+		
+		myList.add(0, "D");
+		System.out.println(myList);
+	
+		myList.addFirst("AA");
+		System.out.println(myList);
+		
+		myList.addLast("AAA");
+		System.out.println(myList);
+	}
+}
